7512115e8fc1fb547062d7cba8c88bf5f640ac71
Gabe Orlanski <gabeorlanski@gmail.com>
2018-02-04 14:50:40 -0500

    Completed Instructions class
    - Added a few more test cases
    - Added in backup xpath feature
    - Made it a bit more streamlined for users

--- src/instructions.py
+++ src/instructions.py
@@ -2,141 +2,216 @@ from lxml import etree
 from io import StringIO
 import os
 import sys
+
 if r'\src' in os.getcwd():
-	sys.path.insert(0, os.path.normpath(os.getcwd() + os.sep + os.pardir))
-	os.chdir(os.path.normpath(os.getcwd() + os.sep + os.pardir))
+    sys.path.insert(0, os.path.normpath(os.getcwd() + os.sep + os.pardir))
+    os.chdir(os.path.normpath(os.getcwd() + os.sep + os.pardir))
 """
 Wrapper for xpath commands that allows the user to easily use xpaths
 """
-basic_html = open(os.getcwd()+r'\tests\instruction_test_html\basic_call.html').read()
+basic_html = open(os.getcwd() + r'\tests\instruction_test_html\basic_call.html').read()
+
+
 class Instruction:
-	# NEVER MODIFY THESE VARIABLES OUTSIDE OF MEMBER FUNCTIONS!
-	_name = None
-	_raw = None
-	_xpath = None
-	_children = []
-	_text = False
-	_attrib = None
-	_getdata = False
-	_id = None
-	_data = None
+    # NEVER MODIFY THESE VARIABLES OUTSIDE OF MEMBER FUNCTIONS!
+    _name = None
+    _raw = None
+    _xpath = None
+    _children = []
+    _text = False
+    _attrib = None
+    _getdata = False
+    _id = None
+    _data = None
+    _debug = False
+    _numresults = 0
+    _backups = None
+    _backupsraw = None
+    parser = None
+    context = None
 
-	parser = None
-	context = None
+    def __init__(self, path, name=None, children=None, text=False, attrib=None, get_data=False, key=None, debug=False, backup_xpaths=[]):
+        """
+        :param path: The XPATH as a string
+        :type path: str
+        :param name: The name of this instruction
+        :type name: str
+        :param children: List of instructions to be executed on the result of this command
+        :type children: [Instruction]
+        :param text: If you need to get the text of the result of the xpath
+        :type text: bool
+        :param attrib: Dictionary of all the attributes from the result that you want to get
+        :type attrib: {name:attribute}
+        :param get_data: If this Instruction has data you want to get
+        :type get_data: bool
+        :param key: the key that the parent will see this instruction as
+        :type key: Whatever data type you want to use as an id
+        :param debug: Enable Debugging
+        :type debug: bool
+        :param backup_xpaths: Any backup xpath to get the same data to deal with any changes in the html from page to page
+        :type backup_xpaths: [str]
+        """
+        self._raw = path
+        self._xpath = etree.XPath(self._raw)
+        self._name = name
+        if children is not None:
+            self._children = children
+        else:
+            self._children = []
+        self._text = text
+        if attrib is not None:
+            self._attrib = attrib
+        else:
+            self._attrib = {}
 
-	def __init__(self, path, name=None, children=None, text=False, attrib=None, get_data=False, key=None):
-		"""
-		:param path: The XPATH as a string
-		:type path: str
-		:param name: The name of this instruction
-		:type name: str
-		:param children: List of instructions to be executed on the result of this command
-		:type children: [Instruction]
-		:param text: If you need to get the text of the result of the xpath
-		:type text: bool
-		:param attrib: List of all the attributes from the result that you want to get
-		:type attrib: [{name:attribute}]
-		:param get_data: If this Instruction has data you want to get
-		:type get_data: bool
-		:param key: the key that the parent will see this instruction as
-		:type key: Whatever data type you want to use as an id
-		"""
-		self._raw = path
-		self._xpath = etree.XPath(self._raw)
-		self._name = name
-		if children is not None:
-			self._children = children
-		else:
-			self._children = []
-		self._text = text
-		if attrib is not None:
-			self._attrib = attrib
-		else:
-			self._attrib = {}
+        self._getdata = get_data
+        self._key = key
+        self._debug = debug
+        if len(backup_xpaths) != 0:
+            self._backupsraw = backup_xpaths
+            self._backups = []
+            for i in self._backupsraw:
+                self._backups.append(etree.XPath(i))
 
-		self._getdata = get_data
-		self._key = key
+    def debug_print(self):
+        """
 
-	def _debug(self):
-		print("-- DEBUGGING --")
-		print("NAME: " + self._name)
-		print("XPATH: " + self._raw)
-		print("len(_CHILDREN): " + str(len(self._children)))
+        :rtype: None
+        """
+        print("-- DEBUGGING --")
+        print("NAME: " + self._name)
+        print("XPATH: " + self._raw)
+        print("len(_CHILDREN): " + str(len(self._children)))
 
-	def addChild(self, i):
-		# TODO: add in checking for duplicate instructions
-		if type(i) != Instruction:
-			raise TypeError("Expected i to by type 'Instruction', instead got: " + str(type(i)))
-		self._children.append(i)
+    def addChild(self, i):
+        # TODO: add in checking for duplicate instructions
+        if type(i) != Instruction:
+            raise TypeError("Expected i to by type 'Instruction', instead got: " + str(type(i)))
+        self._children.append(i)
 
-	def getName(self):
-		return self._name
+    def getName(self):
+        return self._name
 
-	def _retrieveData(self,elem):
-		# TODO: switch this to return a key if need be
-		rtr_dict = dict()
-		if self._text:
-			rtr_dict[self._name] = elem.text.strip()
-		for a in self._attrib.keys():
-			rtr_dict[a] = elem.attrib[self._attrib[a]]
-		for c in self._children:
-			success = c(elem)
-			a = c.data()
-			if success:
-				rtr_dict = {**rtr_dict, **a}
-		return rtr_dict
+    def _retrieveData(self, elem):
 
-	def __call__(self, context):
-		context_use = None
-		if type(context) == str:
-			parser = etree.HTMLParser(encoding="utf-8")
-			context_use = etree.parse(StringIO(context), parser=parser)
-		else:
-			context_use = context
+        rtr_dict = dict()
+        if self._text:
+            rtr_dict[self._name] = elem.text.strip()
+        for a in self._attrib.keys():
+            rtr_dict[a] = elem.attrib[self._attrib[a]]
+        c_data = []
+        for c in self._children:
+            success = c(elem)
+            a = c.data()
 
-		result = self._xpath(context_use)
-		print("".join("-"))
-		print("NAME: " + self._name)
-		for i in result:
-			if type(context) == str:
-				original = context
-			else:
-				original = ""
-			print(etree.tostring(i, pretty_print=True, method='xml').decode('utf-8').strip())
-			print(basic_html.find(etree.tostring(i, pretty_print=True, method='xml').decode('utf-8').strip()))
-		# If there is no results, return false
-		if len(result) == 0:
-			return False
+            if success:
+                c_data.append(a)
+        try:
+            rtr_dict["num_children"] = len(self._children)
+        except:
+            rtr_dict["num_children"] = 0
+        rtr_dict["children"] = c_data
+        return rtr_dict
 
-		if len(result) == 1:
-			result_single = result[0]
-			rtr_dict = self._retrieveData(result_single)
-			self._data = rtr_dict
-			return True
+    def __call__(self, context):
+        if type(context) == str:
+            parser = etree.HTMLParser(encoding="utf-8")
+            context_use = etree.parse(StringIO(context), parser=parser)
+        else:
+            context_use = context
 
-		# Create a dict of the results
-		# TODO: Switch this from list format to a dict format
-		answer = {}
-		for i, j in zip(result, range(len(result))):
-			info_dict = self._retrieveData(i)
-			answer[self._name+"_"+str(j)] = info_dict
+        # Get the results from the xpath
+        result = self._xpath(context_use)
+        self._numresults = len(result)
 
-		self._data = answer
-		return True
+        # Debugging stuff
+        if self._debug:
+            print("".join("-"))
+            print("NAME: " + self._name)
 
-	def key(self):
-		return self._key
+        # If there are no results, try backups. If that fails, return false
+        if len(result) == 0:
+            if not self._backups:
+                return False
 
-	def data(self):
-		return self._data
+            # Iterate through backups until either a result is found, or  it reaches the end
+            backup_iter = iter(self._backups)
+            while len(result) == 0:
+                try:
+                    backup = next(backup_iter)
+                except:
+                    return False
+
+                result = backup(context_use)
+            if len(result) == 0:
+                return False
+
+
+        if self._debug:
+            for i in result:
+                print(etree.tostring(i, pretty_print=True, method='xml').decode('utf-8').strip())
+                print(basic_html.find(etree.tostring(i, pretty_print=True, method='xml').decode('utf-8').strip()))
+
+        data = {}
+        if len(result) == 1:
+            result_single = result[0]
+            data["result_1"] = self._retrieveData(result_single)
+            self._data = data
+            return True
+
+        # Create a dict of the results
+        # TODO: Switch this from list format to a dict format
+
+        for i, j in zip(result, range(len(result))):
+            info_dict = self._retrieveData(i)
+            data["result_" + str(j + 1)] = info_dict
+
+        self._data = data
+        return True
+
+    def key(self):
+        return self._key
+
+    def data(self, minimal=True):
+        if minimal:
+            return self._data
+
+        return dict(num_results=self._numresults, xpath=self._raw, name=self._name, data=self._data)
+
+    def get_format(self, minimal=True):
+        """
+        :return: A mock-up of what the actual format would look like
+        :rtype: dict
+        """
+        rtr = dict(num_results=0, xpath=self._raw, name=self._name, data={})
+        result_data = {}
+        if self._attrib:
+            for i in self._attrib.keys():
+                result_data[i] = "Data from: " + self._attrib[i]
+        if self._text:
+            result_data[self._name] = "Text data"
+
+        if self._children:
+            result_data["num_children"] = len(self._children)
+            c_data = []
+            for c in self._children:
+                child_dct = c.get_format()
+                c_data.append(child_dct)
+
+            result_data["children"] = c_data
+
+        rtr['data']['result_1'] = result_data
+        if minimal:
+            return dict(result_1=result_data)
+        return rtr
 
 
 if __name__ == '__main__':
-	basic_html = open(os.getcwd() + r'\tests\instruction_test_html\basic_call.html').read()
-	# TESTING MULTI CHILDREN
-	a = Instruction("//div[@class='outro']", "parent", text=True)
-	b = Instruction("./h1[@class='child_multi']", "child", text=True)
-	a.addChild(b)
-	worked = a(basic_html)
-	rslt = a.data()
-	print(rslt)
+    basic_html = open(os.getcwd() + r'\tests\instruction_test_html\basic_call.html').read()
+    # TESTING MULTI CHILDREN
+    parent_a = Instruction("//div[@class='single_parent']", "parent", text=True)
+    child_a = Instruction("./div[@class='single_child']", "child", text=True)
+    parent_a.addChild(child_a)
+    success_a = parent_a(basic_html)
+    rslt_a = parent_a.data()
+    print(parent_a.get_format())
--- tests/test_instruction.py
+++ tests/test_instruction.py
@@ -5,114 +5,173 @@ from lxml import etree
 from io import StringIO
 
 if r'\tests' in os.getcwd():
-	sys.path.insert(0, os.path.normpath(os.getcwd() + os.sep + os.pardir))
-	os.chdir(os.path.normpath(os.getcwd() + os.sep + os.pardir))
+    sys.path.insert(0, os.path.normpath(os.getcwd() + os.sep + os.pardir))
+    os.chdir(os.path.normpath(os.getcwd() + os.sep + os.pardir))
 
 from src.instructions import Instruction
 
 
 class TestInstruction(TestCase):
-	def setUp(self):
-		self.basic_html = open(os.getcwd()+r'\tests\instruction_test_html\basic_call.html').read()
-		self.complex_html = open(os.getcwd()+r'\tests\instruction_test_html\complex_tests.html').read()
-	def test_init(self):
-		test = Instruction("//div", "test_name", [Instruction("//a"), Instruction("//b")], True, {"test_attrib_key":"test_attrib"}, True)
-		self.assertEqual(test._raw, "//div")
-		self.assertEqual(test._name, "test_name")
-		self.assertEqual(test._children[0]._raw, "//a")
-		self.assertEqual(test._children[1]._raw, "//b")
-		self.assertEqual(test._children[1]._raw, "//b")
-		self.assertTrue(test._text)
-		self.assertDictEqual(test._attrib, {"test_attrib_key":"test_attrib"})
-		self.assertTrue(test._getdata)
+    def setUp(self):
+        self.basic_html = open(os.getcwd() + r'\tests\instruction_test_html\basic_call.html').read()
+        self.complex_html = open(os.getcwd() + r'\tests\instruction_test_html\complex_tests.html').read()
 
-		test_b = Instruction("//div", "test_b_name")
-		self.assertEqual(test_b._raw, "//div")
-		self.assertEqual(test_b._name, "test_b_name")
-		self.assertEqual(test_b._children, [])
-		self.assertFalse(test_b._text)
-		self.assertEqual(test_b._attrib, {})
-		self.assertFalse(test_b._getdata)
+    def test_init(self):
+        test = Instruction("//div", "test_name", [Instruction("//a"), Instruction("//b")], True, {"test_attrib_key": "test_attrib"}, True)
+        self.assertEqual(test._raw, "//div")
+        self.assertEqual(test._name, "test_name")
+        self.assertEqual(test._children[0]._raw, "//a")
+        self.assertEqual(test._children[1]._raw, "//b")
+        self.assertEqual(test._children[1]._raw, "//b")
+        self.assertTrue(test._text)
+        self.assertDictEqual(test._attrib, {"test_attrib_key": "test_attrib"})
+        self.assertTrue(test._getdata)
 
-	def test_addChild(self):
-		test_parent = Instruction("parent")
-		test_child = Instruction("child")
-		test_error = "error"
-		self.assertEqual(len(test_parent._children), 0)
-		test_parent.addChild(test_child)
-		self.assertEqual(len(test_parent._children), 1)
-		self.assertEqual(test_parent._children[0]._raw, "child")
-		with self.assertRaises(TypeError):
-			test_parent.addChild(test_error)
+        test_b = Instruction("//div", "test_b_name")
+        self.assertEqual(test_b._raw, "//div")
+        self.assertEqual(test_b._name, "test_b_name")
+        self.assertEqual(test_b._children, [])
+        self.assertFalse(test_b._text)
+        self.assertEqual(test_b._attrib, {})
+        self.assertFalse(test_b._getdata)
 
-	def test_basic_call(self):
-		# TESTING BASIC INSTRUCTION
-		basic = Instruction("//p", "test",text=True)
-		success = basic(self.basic_html)
-		returned = basic.data()
-		self.assertTrue(success)
-		self.assertDictEqual(returned,{"test":"Test Basic"})
+    def test_addChild(self):
+        test_parent = Instruction("parent")
+        test_child = Instruction("child")
+        test_error = "error"
+        self.assertEqual(len(test_parent._children), 0)
+        test_parent.addChild(test_child)
+        self.assertEqual(len(test_parent._children), 1)
+        self.assertEqual(test_parent._children[0]._raw, "child")
+        with self.assertRaises(TypeError):
+            test_parent.addChild(test_error)
 
-	def test_no_results(self):
-		# TESTING NO RESULTS
-		nothing = Instruction("//div[@class='blah']","nothing")
-		success = nothing(self.basic_html)
-		returned = nothing.data()
-		self.assertIsNone(returned)
-		self.assertFalse(success)
+    def test_basic_call(self):
+        # TESTING BASIC INSTRUCTION
+        basic = Instruction("//p", "test", text=True)
+        success = basic(self.basic_html)
+        returned = basic.data()
+        self.assertTrue(success)
+        self.assertDictEqual(returned, {'result_1': {'test': '<p>Test Basic</p>', 'num_children': 0, 'children': []}})
 
-	def test_child(self):
-		# TESTING CHILD
-		parent_a = Instruction("//div[@class='intro']","parent",text=True)
-		child_a = Instruction("./h1", "child", text=True)
-		parent_a.addChild(child_a)
-		success_a = parent_a(self.basic_html)
-		rslt_a = parent_a.data()
-		self.assertTrue(success_a)
-		self.assertDictEqual(rslt_a,{'parent': 'Parent', 'child': 'Child'})
+    def test_no_results(self):
+        # TESTING NO RESULTS
+        nothing = Instruction("//div[@class='blah']", "nothing")
+        success = nothing(self.basic_html)
+        returned = nothing.data()
+        self.assertIsNone(returned)
+        self.assertFalse(success)
 
-	def test_child_no_result(self):
-		# TESTING CHILD NO RESULT
-		parent_b = Instruction("//h1[@class='intro']","parent",text=True)
-		child_b = Instruction("./div", "child", text=True)
-		parent_b.addChild(child_b)
-		success_b = parent_b(self.basic_html)
-		rslt_b = parent_b.data()
-		self.assertTrue(success_b)
-		self.assertDictEqual(rslt_b, {'parent': 'Parent'})
+    def test_child(self):
+        # TESTING CHILD
+        parent_a = Instruction("//div[@class='single_parent']", "parent", text=True)
+        child_a = Instruction("./div[@class='single_child']", "child", text=True)
+        parent_a.addChild(child_a)
+        success_a = parent_a(self.basic_html)
+        rslt_a = parent_a.data()
+        self.assertTrue(success_a)
+        self.assertDictEqual(parent_a.get_format(),
+                             {'result_1': {'parent': 'Text data', 'num_children': 1, 'children': [{'result_1': {'child': 'Text data'}}]}})
+        self.assertDictEqual(rslt_a, {
+            'result_1': {'children': [{'result_1': {'children': [], 'child': 'Child', 'num_children': 0}}], 'parent': 'Parent', 'num_children': 1}
+        })
 
-	def test_multi_element(self):
-		# TESTING MULTI ELEMENT
-		multi = Instruction("//h1[@class='outro']","multi",text=True)
-		success_m = multi(self.basic_html)
-		rslt_m = multi.data()
-		self.assertTrue(success_m)
-		self.assertDictEqual(rslt_m,{'multi_0': {'multi': 'Multi Number 1'}, 'multi_1': {'multi': 'Multi Number 2'}})
+    def test_child_no_result(self):
+        # TESTING CHILD NO RESULT
+        parent_b = Instruction("//div[@class='single_parent']", "parent", text=True)
+        child_b = Instruction("./h1", "child", text=True)
+        parent_b.addChild(child_b)
+        success_b = parent_b(self.basic_html)
+        rslt_b = parent_b.data()
+        self.assertTrue(success_b)
+        self.assertDictEqual(rslt_b, {
+            'result_1': {
+                'parent': 'Parent', 'num_children': 1, 'children': []
+            }
+        })
 
-	def test_attributes(self):
-		# TESTING ATTRIBUTES
-		attrib = Instruction("//a","attrib",attrib={'link':'href'})
-		success_c = attrib(self.basic_html)
-		rslt_c = attrib.data()
-		self.assertTrue(success_c)
-		self.assertDictEqual(rslt_c,{'link': 'www.reddit.com'})
+    def test_multi_element(self):
+        # TESTING MULTI ELEMENT
+        multi = Instruction("//div[@class='multi_parent']", "multi", text=True)
+        success_m = multi(self.basic_html)
+        rslt_m = multi.data()
+        self.assertTrue(success_m)
+        self.assertDictEqual(rslt_m, {
+            'result_1': {'children': [], 'multi': 'Multi Number 1', 'num_children': 0},
+            'result_2': {'children': [], 'multi': 'Multi Number 2', 'num_children': 0}
+        })
 
-	def test_child_multi(self):
-		# TESTING MULTI CHILDREN
-		a = Instruction("//h1[@class='outro']","parent", text=True)
-		b = Instruction("./div[@class='child_multi']","child",text=True)
-		a.addChild(b)
-		worked = a(self.basic_html)
-		rslt = a.data()
-		self.assertTrue(worked)
-		self.assertEqual(type(rslt),dict)
-		self.assertDictEqual(rslt,{'parent_0': {'parent': 'Multi Number 1', 'child_0': {'child': 'Child Number 1'}, 'child_1': {'child': 'Child Number 1 2'}}, 'parent_1': {'parent': 'Multi Number 2', 'child': 'Child Number 2'}})
+    def test_attributes(self):
+        # TESTING ATTRIBUTES
+        attrib = Instruction("//a", "attrib", attrib={'link': 'href'})
+        success_c = attrib(self.basic_html)
+        rslt_c = attrib.data()
+        self.assertTrue(success_c)
+        self.assertDictEqual(rslt_c, {'result_1': {'link': 'www.reddit.com', 'children': [], 'num_children': 0}})
 
-	def test_nested_child(self):
-		a = Instruction("//div[@class='nested_children']","parent", text=True)
-		b = Instruction("//h1[@class='nested_children']","child", text=True)
-		c = Instruction("//p[@class='nested_children']","sub_child", text=True)
-		b.addChild(c)
-		a.addChild(b)
-		self.assertTrue(a(self.complex_html))
-		self.assertDictEqual(a.data(),{'parent': 'Parent', 'child': 'Child', 'sub_child': 'Sub Child'})
+    def test_child_multi(self):
+        # TESTING MULTI CHILDREN
+        a = Instruction("//div[@class='multi_parent']", "parent", text=True)
+        b = Instruction("./div[@class='multi_child']", "child", text=True)
+        a.addChild(b)
+        worked = a(self.basic_html)
+        rslt = a.data()
+        self.assertTrue(worked)
+        self.assertEqual(type(rslt), dict)
+        self.assertDictEqual(rslt, {
+            'result_2'   : {
+                'children': [{'result_1': {'children': [], 'child': 'Child Number 2', 'num_children': 0}}], 'num_children': 1,
+                'parent'  : 'Multi Number 2'
+            }, 'result_1': {
+                'children'        : [{
+                    'result_2': {'children': [], 'child': 'Child Number 1 2', 'num_children': 0},
+                    'result_1': {'children': [], 'child': 'Child Number 1', 'num_children': 0}
+                }], 'num_children': 1, 'parent': 'Multi Number 1'
+            }
+        })
+
+    def test_nested_child(self):
+        a = Instruction("//div[@class='nested_children']", "parent", text=True)
+        b = Instruction("//h1[@class='nested_children']", "child", text=True)
+        c = Instruction("//p[@class='nested_children']", "sub_child", text=True)
+        b.addChild(c)
+        a.addChild(b)
+        self.assertTrue(a(self.complex_html))
+        self.assertDictEqual(a.data(), {
+            'result_1': {
+                'children'  : [{
+                    'result_1': {
+                        'children': [{'result_1': {'sub_child': 'Sub Child', 'children': [], 'num_children': 0}}], 'child': 'Child', 'num_children': 1
+                    }
+                }], 'parent': 'Parent', 'num_children': 1
+            }
+        })
+
+    def test_backup(self):
+        # TESTING BASIC INSTRUCTION
+        basic = Instruction("//div[@class='test']", "test", text=True, backup_xpaths=["//p"])
+        success = basic(self.basic_html)
+        returned = basic.data()
+        self.assertTrue(success)
+        self.assertDictEqual(returned, {'result_1': {'test': '<p>Test Basic</p>', 'num_children': 0, 'children': []}})
+
+    def test_multi_child_backup(self):
+        # TESTING MULTI CHILDREN
+        a = Instruction("//div[@class='doesnotexist']", "parent", text=True, backup_xpaths=["//div[@class='multi_parent']"])
+        b = Instruction("//div[@class='test2iguess']", "child", text=True, backup_xpaths=["./div[@class='multi_child']"])
+        a.addChild(b)
+        worked = a(self.basic_html)
+        rslt = a.data()
+        self.assertTrue(worked)
+        self.assertEqual(type(rslt), dict)
+        self.assertDictEqual(rslt, {
+            'result_2'   : {
+                'children': [{'result_1': {'children': [], 'child': 'Child Number 2', 'num_children': 0}}], 'num_children': 1,
+                'parent'  : 'Multi Number 2'
+            }, 'result_1': {
+                'children'        : [{
+                    'result_2': {'children': [], 'child': 'Child Number 1 2', 'num_children': 0},
+                    'result_1': {'children': [], 'child': 'Child Number 1', 'num_children': 0}
+                }], 'num_children': 1, 'parent': 'Multi Number 1'
+            }
+        })
